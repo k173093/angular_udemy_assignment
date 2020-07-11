dd0f53f923dd646d6089c5f2a1928ba246cdc50d
k173093 <68111358+k173093@users.noreply.github.com>
2020-07-11 13:38:23 +0500

    project completed.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PersonalDetailsComponent } from './personal-details/personal-details.component';
 import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { WizardFinishedComponent } from './wizard-finished/wizard-finished.component';
 
 
 const routes: Routes = [
-  {path:"dashboard",component:PersonalDetailsComponent},
-  {path:"about",component:ContactDetailsComponent},
-  {path:"",redirectTo:"dashboard", pathMatch:"full"}
+  {path:"personal",component:PersonalDetailsComponent},
+  {path:"contact",component:ContactDetailsComponent},
+  {path:"skills",component:SkillsComponent},
+  {path:"wexp",component:WorkExperienceComponent},
+  {path:"wfinish",component:WizardFinishedComponent},
+  {path:"",redirectTo:"personal", pathMatch:"full"}
 ];
 
 @NgModule({
